a802c86a87b1804864235b9161f92968c7e008bb
juniper712 <jenn712@gmail.com>
2022-09-02 11:58:40 -0700

    Main

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-// Write your JavaScript code here!
+
+
+const { addDestinationInfo } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       let chosenPlanet = choosePlanet(listedPlanets);
+       addDestinationInfo(document, chosenPlanet.name, chosenPlanet.diamater, chosenPlanet.star, chosenPlanet.distance, chosenPlanet.moon, chosenPlanet.moons, chosenPlanet.image);
    })
-   
+  
+    form.addEventListener("submit", function(event) {
+
+        let pilotInput = document.querySelector("input[name=pilotName]");
+        let pilot = pilotInput.value;
+        let copilotInput = document.querySelector("input[name=copilotName]");
+        let colpilot = copilotInput.value;
+        let fuelInput = document.querySelector("input=[number=fuelLevel] ");
+        let fuelLevel = Number(fuelLevel.value);
+        let cargoInput = document.querySelector("input=[number=cargoMass]");
+        let cargoLevel = Number(cargoMass.value);
+
+        formsSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+
+   event.preventDefault();
+    });
+
 });
